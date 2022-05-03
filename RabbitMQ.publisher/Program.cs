1597b20b0c960599a3e674b8e5888c34cfd0881e
dogustuluk <dogus.tuluk@gmail.com>
2022-05-03 17:37:56 +0300

    publisher created

--- RabbitMQ.publisher/Program.cs
+++ RabbitMQ.publisher/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using System;
+using System.Linq;
+using System.Text;
 
 namespace RabbitMQ.publisher
 {
@@ -6,7 +9,26 @@ namespace RabbitMQ.publisher
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://nxdranwu:n_3nr-xZlXx0NoCWuFP05gTqZfp7_hwK@sparrow.rmq.cloudamqp.com/nxdranwu ");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            channel.ExchangeDeclare("logs-direct", durable: true, type: ExchangeType.Direct);
+
+            Enumerable.Range(1, 50).ToList().ForEach(x =>
+             {
+                 var message = $"log {x}";
+                 var messageBody = Encoding.UTF8.GetBytes(message);
+
+                 channel.BasicPublish("logs-direct", "", null, messageBody);
+
+                 Console.WriteLine($"Mesaj Gönderilmiştir {message}");
+             });
+
+            Console.ReadLine();
         }
     }
 }
